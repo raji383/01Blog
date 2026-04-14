3a0775b62ae8c84371f9a11449c547f49fc0ed4e
azraji <zdine30@gmail.com>
2026-04-14 12:56:53 +0100

    re start

--- backend/src/main/java/com/example/demo/dto/CommentRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record CommentRequest(
-    @NotBlank @Size(max = 1000) String content
-) {}
