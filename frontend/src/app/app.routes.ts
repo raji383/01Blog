37c8da103c3016d22efeaa90b9091f012949dcd9
azraji <zdine30@gmail.com>
2026-04-25 17:22:18 +0100

    add Dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NotFoundComponent } from './not-found-component/not-found-component';
 import { Home } from './home/home';
 import { Auth } from './auth/auth';
 import { Profile } from './profile/profile';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
     { path: '', component: Home },
     {path:'login',component:Auth},
     {path:'profile/:id',component:Profile},
+    {path:'admin',component:Dashboard},
     { path: '**', component: NotFoundComponent }
 
 ];
